c2873b874dcfe365144337c6899896798d7c7a8f
setyawanackerman <bayu.setyawan199@sma.belajar.id>
2026-05-03 02:09:18 +0700

    fix: toolbar visibility and svg icons for theme toggle

--- api/app.py
+++ api/app.py
@@ -1,14 +1,12 @@
 from flask import Flask, render_template
 import os
 
-# Mengarahkan Flask agar bisa membaca folder templates dan static di luar folder api
 app = Flask(__name__, 
             template_folder='../templates', 
             static_folder='../static')
 
 @app.route('/')
 def home():
-    # Data diri untuk mahasiswa Informatika
     data_diri = {
         "nama_lengkap": "Bayu Setyawan",
         "npm": "50425194",
