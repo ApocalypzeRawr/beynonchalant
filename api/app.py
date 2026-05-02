96b76a7741025b56d5cf79920fae03f0d3c09216
setyawanackerman <bayu.setyawan199@sma.belajar.id>
2026-05-03 01:56:50 +0700

    feat: add dark and light mode toggle

--- api/app.py
+++ api/app.py
@@ -1,16 +1,17 @@
 from flask import Flask, render_template
 import os
 
-# Update bagian ini
+# Mengarahkan Flask agar bisa membaca folder templates dan static di luar folder api
 app = Flask(__name__, 
             template_folder='../templates', 
             static_folder='../static')
 
 @app.route('/')
 def home():
+    # Data diri untuk mahasiswa Informatika
     data_diri = {
         "nama_lengkap": "Bayu Setyawan",
         "npm": "50425194",
-        "deskripsi": "I am an enthusiastic Information Technology student..."
+        "deskripsi": "I am an enthusiastic Information Technology student, passionate about building robust systems with Java and Python."
     }
     return render_template('index.html', data=data_diri)
